5e8eb5d3a68583e69f2b05535340f8497a8060ca
C.W. Betts <computers57@hotmail.com>
2021-02-12 05:05:44 -0700

    Quiet a warning. Add size declarations to the MIDI constants.

--- Boxer/BXMIDIConstants.h
+++ Boxer/BXMIDIConstants.h
@@ -5,74 +5,85 @@
  online at [http://www.gnu.org/licenses/gpl-2.0.txt].
  */
 
+#import <Foundation/Foundation.h>
+
 
 //MIDI-related constants for General MIDI and MT-32 messages.
+NS_ENUM(uint8_t) {
+    BXSysexStart = 0xF0,
+    BXSysexEnd = 0xF7,
 
-#define BXSysexStart 0xF0
-#define BXSysexEnd 0xF7
-
-/// MIDI messages use 7 bits, but must be sent via byte arrays.
-/// This mask clears the 8th and higher bits of the byte.
-#define BXMIDIBitmask 0x7F
+    /// MIDI messages use 7 bits, but must be sent via byte arrays.
+    /// This mask clears the 8th and higher bits of the byte.
+    BXMIDIBitmask = 0x7F
+};
 
 
 #pragma mark -
 #pragma mark General MIDI sysex message codes
 
-#define BXGeneralMIDISysexNonRealtime 0x7E
-#define BXGeneralMIDISysexRealtime 0x7F
+NS_ENUM(uint8_t) {
+    BXGeneralMIDISysexNonRealtime = 0x7E,
+    BXGeneralMIDISysexRealtime = 0x7F,
 
-#define BXGeneralMIDISysexAllChannels 0x7F
-#define BXGeneralMIDISysexDeviceControl 0x04
-#define BXGeneralMIDISysexMasterVolume 0x01
+    BXGeneralMIDISysexAllChannels = 0x7F,
+    BXGeneralMIDISysexDeviceControl = 0x04,
+    BXGeneralMIDISysexMasterVolume = 0x01,
 
 #define BXGeneralMIDIMaxMasterVolume 0x3FFF //16383
 
 
-#define BXSysexManufacturerIDRoland 0x41
-#define BXSysexManufacturerIDNonRealtime 0x7E
-#define BXSysexManufacturerIDRealtime 0x7F
-
+    BXSysexManufacturerIDRoland = 0x41,
+    BXSysexManufacturerIDNonRealtime = 0x7E,
+    BXSysexManufacturerIDRealtime = 0x7F,
+};
 
 #pragma mark -
 #pragma mark Roland sysex message format
 
-//Start byte, manufacturer ID, device ID, model ID, message Type
-#define BXRolandSysexHeaderLength 5
-//High byte, middle byte, low byte
-#define BXRolandSysexAddressLength 3
-//High byte, middle byte, low byte
-#define BXRolandSysexRequestSizeLength 3
-//Checksum, end byte
-#define BXRolandSysexTailLength 2
+//! Start byte, manufacturer ID, device ID, model ID, message Type
+#define BXRolandSysexHeaderLength 5l
+//! High byte, middle byte, low byte
+#define BXRolandSysexAddressLength 3l
+//! High byte, middle byte, low byte
+#define BXRolandSysexRequestSizeLength 3l
+//!cChecksum, end byte
+#define BXRolandSysexTailLength 2l
 
 
 #define BXRolandSysexChecksumModulus 128
 
-#define BXRolandSysexModelIDMT32 0x16
-#define BXRolandSysexModelIDD50 0x14
+NS_ENUM(uint8_t) {
 
-#define BXRolandSysexDeviceIDDefault 0x10
+    BXRolandSysexModelIDMT32 = 0x16,
+    BXRolandSysexModelIDD50 = 0x14,
 
-#define BXRolandSysexRequest 0x11
-#define BXRolandSysexSend 0x12
+    BXRolandSysexDeviceIDDefault = 0x10,
 
-#define BXRolandSysexSendMinLength (BXRolandSysexHeaderLength + BXRolandSysexAddressLength + BXRolandSysexTailLength)
+    BXRolandSysexRequest = 0x11,
+    BXRolandSysexSend = 0x12
+};
 
-#define BXRolandSysexRequestLength (BXRolandSysexSendMinLength + BXRolandSysexRequestSizeLength)
+NS_ENUM(NSInteger) {
+    BXRolandSysexSendMinLength = (BXRolandSysexHeaderLength + BXRolandSysexAddressLength + BXRolandSysexTailLength),
+
+    BXRolandSysexRequestLength = (BXRolandSysexSendMinLength + BXRolandSysexRequestSizeLength)
+};
 
 #pragma mark -
 #pragma mark MT-32-specific sysex message parameters
 
 #define BXMT32MaxMasterVolume 100
-#define BXMT32LCDMessageLength 20
+#define BXMT32LCDMessageLength 20l
 
-#define BXMT32SysexAddressPatchMemory 0x05
-#define BXMT32SysexAddressTimbreMemory 0x08
-#define BXMT32SysexAddressSystemArea 0x10
-#define BXMT32SysexAddressReset 0x7F
-#define BXMT32SysexAddressDisplay 0x20
+NS_ENUM(uint8_t) {
+    BXMT32SysexAddressPatchMemory = 0x05,
+    BXMT32SysexAddressTimbreMemory = 0x08,
+    BXMT32SysexAddressSystemArea = 0x10,
+    BXMT32SysexAddressReset = 0x7F,
+    BXMT32SysexAddressDisplay = 0x20,
 
 
-#define BXChannelModeChangePrefix 0xB0
-#define BXAllNotesOffMessage 0x7B
+    BXChannelModeChangePrefix = 0xB0,
+    BXAllNotesOffMessage = 0x7B
+};
